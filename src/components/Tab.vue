f3c2d0f2a619b6a1c7e8db73b0d5846cb5d990b1
mxylovezh <18322276773@163.com>
2020-09-28 19:40:24 +0800

    add components

---
+++ src/components/Tab.vue
@@ -0,0 +1,5 @@
+<template>
+<div>
+  I am Tab
+</div>
+</template>
--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -6,7 +6,7 @@
       <h2>组件列表</h2>
       <ol>
         <li>
-          <router-link to="/doc/swich">Swich 组件</router-link>
+          <router-link to="/doc/switch">Swich 组件</router-link>
         </li>
         <li>
           <router-link to="/doc/button">Button 组件</router-link>
@@ -15,11 +15,13 @@
           <router-link to="/doc/dialog">Dialog 组件</router-link>
         </li>
         <li>
-          <router-link to="/doc/tabs">Tab 组件</router-link>
+          <router-link to="/doc/tab">Tab 组件</router-link>
         </li>
       </ol>
     </aside>
-    <main>主内容</main>
+    <main>
+      <router-view />
+    </main>
   </div>
 </div>
 </template>
