75e4ed1d9f5557ad3a0b66548177c6b67dfd5835
mxylovezh <18322276773@163.com>
2020-10-07 09:48:53 +0800

    添加 dialog组件css样式

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,5 +1,13 @@
 <template>
-<div>
-  I am dialog
-</div>
+  <div>
+    <Dialog/>
+  </div>
 </template>
+<script lang="ts">
+  import Dialog from '../lib/Dialog.vue';
+
+  export default {
+    components: {Dialog}
+  };
+
+</script>
