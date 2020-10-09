d46adf843169f2c320594850a470f5571410078f
mxylovezh <18322276773@163.com>
2020-10-09 17:18:26 +0800

    修改 switchdmo 页面

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,19 +1,94 @@
 <template>
-  <Switch v-model:checked="checked"/>
+<div>
+  <div class="switch-demo-1">
+    <div class="explain">
+      <h4>基本用法</h4>
+      <p>最简单用法</p>
+    </div>
+    <div class="display">
+      <Switch v-model:checked="checked" />
+      <div class="icon">
+        <Icon type="icon-copy" />
+        <Icon type="icon-accesskeys" />
+      </div>
+    </div>
+  </div>
+  <div class="switch-demo-1">
+    <div class="explain">
+      <h4>不可用</h4>
+      <p>Switch 失效状态</p>
+    </div>
+    <div class="display">
+      <Switch v-model:checked="checked" disable />
+      <div class="icon">
+        <Icon type="icon-copy" />
+        <Icon type="icon-accesskeys" />
+      </div>
+    </div>
+  </div>
+</div>
 </template>
 
 <script lang="ts">
-  import Switch from '../lib/Switch.vue';
-  import {ref} from 'vue'
-  export default {
-    components: {
-      Switch,
-    },
-    setup() {
-      const checked = ref(false);
-      return {
-        checked,
-      };
-    },
-  };
+import Switch from '../lib/Switch.vue'
+import Icon from '../lib/Icon.vue'
+import {
+  ref
+} from 'vue'
+export default {
+  components: {
+    Switch,
+    Icon,
+  },
+  setup() {
+    const checked = ref(false)
+    return {
+      checked,
+    }
+  },
+}
 </script>
+
+<style lang="scss">
+.switch-demo-1 {
+  border-radius: 4px;
+  margin: 10px;
+  width: 20em;
+  border: 1px solid #ccc;
+  box-shadow: 0 0 3px fade_out(black, 0.8);
+
+  .display {
+    padding: 20px 10px;
+    display: flex;
+    align-content: center;
+    justify-content: space-between;
+
+    .icon {
+      cursor: pointer;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+
+    .iconfont {
+      font-size: 18px;
+      margin-right: 4px;
+      color: #b3b3b3;
+    }
+  }
+
+  .explain {
+    padding: 10px;
+
+    >h3 {
+      color: #777777;
+    }
+
+    p {
+      padding: 6px 0;
+      color: #314659;
+      font-size: 14px;
+    }
+  }
+}
+</style>
--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,58 +1,58 @@
 <template>
-  <button class="po-switch" :class="{ 'po-checked':checked }">
-    <span @click="toggle"/>
+  <button class="po-switch" :class="{ 'po-checked': checked }">
+    <span @click="toggle" />
   </button>
 </template>
 
 <script lang="ts">
-  export default {
-    props: {
-      checked: Boolean,
-    },
-    setup(props, context) {
-      const toggle = () => {
-        context.emit('update:checked', !props.checked);
-      };
-      return {
-        toggle,
-      };
-    },
-  };
+export default {
+  props: {
+    checked: Boolean,
+  },
+  setup(props, context) {
+    const toggle = () => {
+      context.emit('update:checked', !props.checked)
+    }
+    return {
+      toggle,
+    }
+  },
+}
 </script>
 
 <style lang="scss">
-  $h: 22px;
-  $h2: $h - 4px;
+$h: 22px;
+$h2: $h - 4px;
 
-  .po-switch {
-    height: $h;
-    width: $h * 2;
-    border: none;
-    background: #162229;
-    border-radius: $h/2;
-    position: relative;
+.po-switch {
+  height: $h;
+  width: $h * 2;
+  border: none;
+  background: #162229;
+  border-radius: $h/2;
+  position: relative;
 
-    > span {
-      position: absolute;
-      top: 2px;
-      left: 2px;
-      height: $h2;
-      width: $h2;
-      background: white;
-      border-radius: $h2/2;
-      transition: left 250ms;
-    }
-
-    &.po-checked {
-      background: #aa853b;
-    }
-
-    &.po-checked > span {
-      left: calc(100% - #{$h2} - 2px);
-    }
-
-    &:focus {
-      outline: none;
-    }
+  > span {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background: white;
+    border-radius: $h2/2;
+    transition: left 250ms;
   }
+
+  &.po-checked {
+    background: #aa853b;
+  }
+
+  &.po-checked > span {
+    left: calc(100% - #{$h2} - 2px);
+  }
+
+  &:focus {
+    outline: none;
+  }
+}
 </style>
