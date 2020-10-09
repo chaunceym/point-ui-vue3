76fcd5ce974dd80f150679c39d57df0a06ef5607
mxylovezh <18322276773@163.com>
2020-10-09 14:18:45 +0800

    加入新页面

--- src/router.ts
+++ src/router.ts
@@ -1,25 +1,33 @@
-import {createWebHashHistory, createRouter} from 'vue-router';
-import Home from './views/Home.vue';
-import Doc from './views/Doc.vue';
-import Switch from './components/SwitchDemo.vue';
-import Button from './components/ButtonDemo.vue';
-import Tab from './components/TabDemo.vue';
-import Dialog from './components/DialogDemo.vue';
-import Icon from './components/IconDemo.vue';
+import { createWebHashHistory, createRouter } from 'vue-router'
+import Home from './views/Home.vue'
+import Doc from './views/Doc.vue'
+import Switch from './components/SwitchDemo.vue'
+import Button from './components/ButtonDemo.vue'
+import Tab from './components/TabDemo.vue'
+import Dialog from './components/DialogDemo.vue'
+import Icon from './components/IconDemo.vue'
+import Introduce from './views/Introduce.vue'
+import Install from './views/Install.vue'
+import GetStarted from './views/GetStarted.vue'
 
-const history = createWebHashHistory();
+const history = createWebHashHistory()
 export const router = createRouter({
   history: history,
   routes: [
-    {path: '/', component: Home},
+    { path: '/', component: Home },
     {
-      path: '/doc', component: Doc, children: [
-        {path: 'switch', component: Switch},
-        {path: 'button', component: Button},
-        {path: 'tab', component: Tab},
-        {path: 'dialog', component: Dialog},
-        {path: 'icon', component: Icon},
-      ]
+      path: '/doc',
+      component: Doc,
+      children: [
+        { path: 'introduce', component: Introduce },
+        { path: 'install', component: Install },
+        { path: 'get-started', component: GetStarted },
+        { path: 'switch', component: Switch },
+        { path: 'button', component: Button },
+        { path: 'tab', component: Tab },
+        { path: 'dialog', component: Dialog },
+        { path: 'icon', component: Icon },
+      ],
     },
   ],
-});
+})
--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,125 +1,136 @@
 <template>
-  <div class="layout">
-    <TopNav class="nav"/>
-    <div class="content">
-      <aside v-if="menuVisible">
-        <h2>组件列表</h2>
-        <ol>
-          <li>
-            <router-link to="/doc/switch">Swich 组件</router-link>
-          </li>
-          <li>
-            <router-link to="/doc/button">Button 组件</router-link>
-          </li>
-          <li>
-            <router-link to="/doc/dialog">Dialog 组件</router-link>
-          </li>
-          <li>
-            <router-link to="/doc/tab">Tab 组件</router-link>
-          </li>
-          <li>
-            <router-link to="/doc/icon">Icon 组件</router-link>
-          </li>
-        </ol>
-      </aside>
-      <main>
-        <router-view/>
-      </main>
-    </div>
+<div class="layout">
+  <TopNav class="nav" />
+  <div class="content">
+    <aside v-if="menuVisible">
+      <h2>开始使用</h2>
+      <ol>
+        <li>
+          <router-link to="/doc/introduce">介绍</router-link>
+        </li>
+        <li>
+          <router-link to="/doc/install">安装</router-link>
+        </li>
+        <li>
+          <router-link to="/doc/get-started">开始</router-link>
+        </li>
+      </ol>
+      <h2>组件列表</h2>
+      <ol>
+        <li>
+          <router-link to="/doc/switch">Swich 组件</router-link>
+        </li>
+        <li>
+          <router-link to="/doc/button">Button 组件</router-link>
+        </li>
+        <li>
+          <router-link to="/doc/dialog">Dialog 组件</router-link>
+        </li>
+        <li>
+          <router-link to="/doc/tab">Tab 组件</router-link>
+        </li>
+        <li>
+          <router-link to="/doc/icon">Icon 组件</router-link>
+        </li>
+      </ol>
+    </aside>
+    <main>
+      <router-view />
+    </main>
   </div>
+</div>
 </template>
 
 <script lang="ts">
-  import {
-    inject,
-    Ref,
-    ref
-  } from 'vue';
-  import TopNav from '../components/TopNav.vue';
-  import {
-    router
-  } from '../router';
+import {
+  inject,
+  Ref,
+  ref
+} from "vue";
+import TopNav from "../components/TopNav.vue";
+import {
+  router
+} from "../router";
 
-  export default {
-    components: {
-      TopNav,
-    },
-    setup() {
-      const menuVisible = inject<Ref<boolean>>('xxx');
-      return {
-        menuVisible,
-      };
-    },
-  };
+export default {
+  components: {
+    TopNav,
+  },
+  setup() {
+    const menuVisible = inject < Ref < boolean >> ("xxx");
+    return {
+      menuVisible,
+    };
+  },
+};
 </script>
 
 <style lang="scss">
-  @import '../var';
+@import "../var";
 
-  .layout {
-    display: flex;
-    flex-direction: column;
-    height: 100vh;
+.layout {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
 
-    > .nav {
-      flex-shrink: 0;
-    }
-
-    > .content {
-      flex-grow: 1;
-      padding-top: 60px;
-      padding-left: 156px;
-
-      @media (max-width: 500px) {
-        padding-left: 0;
-      }
-    }
+  >.nav {
+    flex-shrink: 0;
   }
 
-  .content {
-    display: flex;
+  >.content {
+    flex-grow: 1;
+    padding-top: 60px;
+    padding-left: 156px;
 
-    > aside {
-      flex-shrink: 0;
-    }
-
-    > main {
-      overflow: auto;
-      flex-grow: 1;
-      padding: 16px;
-      background: $point-white;
+    @media (max-width: 500px) {
+      padding-left: 0;
     }
   }
+}
 
-  aside {
-    color: $point-color;
-    background: $point-bg;
-    width: 150px;
-    position: fixed;
-    top: 0;
-    left: 0;
-    padding: 70px 8px 8px;
-    height: 100%;
+.content {
+  display: flex;
 
-    > h2 {
-      margin: 10px;
-    }
-
-    > ol {
-      > li {
-        padding: 10px 8px;
-        margin: 4px 0;
-        transition: all 250ms;
-        border-radius: 4px 50% 50% 4px;
-      }
-
-      > li:hover {
-        background: $point-color;
-        color: $point-bg;
-        transform: translate(15%, 10%);
-        font-weight: 700;
-      }
-    }
+  >aside {
+    flex-shrink: 0;
   }
 
+  >main {
+    overflow: auto;
+    flex-grow: 1;
+    padding: 16px;
+    background: $point-white;
+  }
+}
+
+aside {
+  color: $point-color;
+  background: $point-bg;
+  width: 150px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  padding: 70px 8px 8px;
+  height: 100%;
+
+  >h2 {
+    margin: 10px;
+  }
+
+  >ol {
+    >li {
+      padding: 10px 8px;
+      margin: 4px 0;
+      transition: all 250ms;
+      border-radius: 4px 50% 50% 4px;
+    }
+
+    >li:hover {
+      background: $point-color;
+      color: $point-bg;
+      transform: translate(15%, 10%);
+      font-weight: 700;
+    }
+  }
+}
 </style>
