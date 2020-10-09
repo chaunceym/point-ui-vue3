64ef7fb57ba25a03bf27e761b69577b9ce4ff10d
mxylovezh <18322276773@163.com>
2020-10-09 12:21:53 +0800

    优化首页

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,15 @@
 <div>
   <TopNav />
   <div class="banner">
-    <h1 class="title">Point UI</h1>
-    <h3>基于 Vue3 的轻量级UI框架</h3>
-    <p class="actions">
-      <a href="https://github.com/chaunceym">GitHub</a>
-      <router-link to="/doc">开始</router-link>
-    </p>
+    <div class="banner-left">
+      <h1 class="title">Point UI</h1>
+      <h1>基于 Vue3 的轻量级UI框架</h1>
+      <p class="actions">
+        <a href="https://github.com/chaunceym">GitHub</a>
+        <router-link to="/doc">start</router-link>
+      </p>
+    </div>
+    <div class="banner-right"></div>
   </div>
 </div>
 </template>
@@ -22,35 +25,56 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "../var";
+
 .banner {
   color: #fff;
   padding: 100px 0;
   display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
   background: #1b1b1d;
   height: 100vh;
 
-  >.title {
-    padding-bottom: 40px;
+  &-left {
+    color: $point-color;
+    padding: 50px 100px;
+
+    >.title {
+      padding-bottom: 40px;
+      font-size: 80px;
+    }
+
+    >.actions {
+      padding: 40px 0;
+      margin-top: 40px;
+
+      a {
+        &:first-child {
+          margin-right: 12px;
+          color: $point-color;
+          background: $point-bg;
+        }
+
+        background: $point-color;
+        display: inline-block;
+        color: $point-bg;
+        $h: 48px;
+        text-align: center;
+        border: 1px solid $point-color;
+        height: $h;
+        line-height: $h;
+        border-radius: $h/2;
+        padding: 0 28px;
+
+        &:hover {
+          border: 1px solid lighten($point-color, 15%);
+          color: lighten($point-color, 45%);
+        }
+      }
+    }
   }
 
-  >.actions {
-    padding: 40px 0;
-
-    a {
-      margin: 0 12px;
-      background: #1b1b1d;
-      display: inline-block;
-      $h: 32px;
-      text-align: center;
-      height: $h;
-      line-height: $h;
-      border: 1px solid #fff;
-      border-radius: $h/2;
-      padding: 0 8px;
-    }
+  &-right {
+    width: 50%;
   }
 }
 </style>
